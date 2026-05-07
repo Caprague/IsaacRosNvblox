b2d4acc68cd6423de7f019324bc0092389d70b8f
Caprague <2210134086@qq.com>
2026-05-07 16:01:12 +0800

    fix publish z-axis offset bug

--- sh_list/start_nvblox.sh
+++ sh_list/start_nvblox.sh
@@ -16,7 +16,7 @@ tmux split-window -h -t "$SESSION_NAME:main.0"
 
 # 右 pane: source 后等待 16s，再启动 safety_guardian
 tmux send-keys -t "$SESSION_NAME:main.1" \
-  "source /opt/ros/humble/setup.bash && source /workspaces/isaac_ros-dev/install/setup.bash && sleep 16 && ros2 run safety_guardian safety_guardian_node" \
+  "source /opt/ros/humble/setup.bash && source /workspaces/isaac_ros-dev/install/setup.bash && sleep 18 && ros2 run safety_guardian safety_guardian_node" \
   C-m
 
 # 附加到会话
